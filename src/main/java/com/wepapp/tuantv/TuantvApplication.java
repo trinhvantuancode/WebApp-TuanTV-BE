0e6fdbaa63996226a77c98f0b0de0aa1935f6879
Tiny Sun <tuantv281099@gmail.com>
2024-04-20 23:32:37 +0700

    create project spring boot

---
+++ src/main/java/com/wepapp/tuantv/TuantvApplication.java
@@ -0,0 +1,13 @@
+package com.wepapp.tuantv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuantvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TuantvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wepapp/tuantv/TuantvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wepapp.tuantv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TuantvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
